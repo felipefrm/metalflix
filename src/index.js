730e0d0a595010db3f4304863356f5db444be676
Felipe Melo <narulee@hotmail.com>
2020-07-28 22:38:03 -0300

    routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+// Desafio master blaster na descrição
+// Colocar um jogo ou brincadeira nessa página:
+const Pagina404 = () => (
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+    <h1>Ops, quebrou tudo :O</h1>
+    <p>
+      <a href="/">Você pode jogar ou voltar pra home :)</a>
+    </p>
+   
+    <iframe
+      title="Flappy Bird Game"
+      src="https://mariosouto.com/flappy-bird-devsoutinho/"
+      width="340"
+      height="500"
+      scrolling="no" />
+  </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414" }}>
       <Menu />
@@ -13,6 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Master of Puppets é o terceiro álbum de estúdio lançado pela banda de thrash metal/heavy metal norte-americana Metallica, lançado em 3 de março de 1986 pela Elektra Records."}
       />
 
       <Carousel
@@ -45,4 +46,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
